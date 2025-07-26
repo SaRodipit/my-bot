ab3a66c09791a4d0b5b74fcac56e3fd6cc8c373d
SaRodipit <slavabaymeev@gmail.com>
2025-07-27 05:35:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '7600665438:AAHXDVepbk1QylzY9atnkqmkHXAGD5--TMY';
+const token = '';
 
 const bot = new TelegramBot(token, { polling: true });
 
